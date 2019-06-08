e9f9ba88ad1313289d7e96c15b206eca13a78a85
Pranav Dixit <36262712+dprnav@users.noreply.github.com>
2019-06-08 13:35:36 +0530

    init

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <br/ >Server IP: <?php echo $_SERVER['SERVER_ADDR']; ?>
+        <br/ >Pranav Prateek Server IP: <?php echo $_SERVER['SERVER_ADDR']; ?>
     </body>
 </html>
